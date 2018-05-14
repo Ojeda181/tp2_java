b2b0c1640d8bcf602589d339b5ed5f7aa29b3a09
Leandro <leandro.ojeda181@gmail.com>
2018-05-14 10:41:25 -0300

    Commit ej4

---
+++ Ej4.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Ej4 {
+	public static void main (String[] ar) {
+		int i;
+		int x = 1;
+		while(x < 11){
+			i = 5 * x;
+			System.out.println("5 x " + x + " = " + i);
+			x = x + 1;
+		}
+	}
+}
